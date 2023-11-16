720366c1063bfb27cdf7496623c0bf7da473b21f
shmuir <shmuir1@gmail.com>
2023-11-16 00:26:38 +0000

    data cleaning and testing

--- data_fetch.R
+++ data_fetch.R
@@ -5,20 +5,36 @@ library(BIEN)
 library(rredlist)
 library(janitor)
 
+
+
+
 # leaf photosynthetic rate per leaf area	
 
 lpr <- BIEN_trait_trait("leaf photosynthetic rate per leaf area") %>%
-  rename(scientific_name = scrubbed_species_binomial)
+  rename(scientific_name = scrubbed_species_binomial)  %>%
+  mutate(trait_value = as.numeric(trait_value)) %>%
+  filter(trait_value < 70)
+
+lpr_summary <- lpr %>%
+  group_by(scientific_name) %>%
+  summarise(avg_photo = mean(trait_value, na.rm = TRUE))
 
 
 redlist <- read_csv("data/redlist_species_data/assessments.csv") %>%
-  clean_names()
+  clean_names() %>%
+  filter(population_trend %in% c("Stable", "Increasing", "Decreasing")) %>%
+  mutate(pop_trend = case_when(population_trend == "Decreasing" ~ "Decreasing",
+                               population_trend %in% c("Stable", "Increasing") ~ "Stable_Inc"),
+         pop_trend_binary = case_when(pop_trend == "Decreasing" ~ 0,
+                                      pop_trend == "Stable_Inc" ~ 1)) 
 
 
 #setdiff(lpr$scrubbed_species_binomial, redlist$scientific_name)
 
-join <- inner_join(lpr, redlist, by = "scientific_name")
+join <- inner_join(lpr_summary, redlist, by = "scientific_name")
 
+
+#summary(glm(population_trend ~ trait_value, join))
 
 
 # # Identify token for accessing IUCN API
